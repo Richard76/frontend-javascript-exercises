410a53420e6d7ce067951a7091602a64fb18822c
Richard <RichardFarr7@gmail.com>
2014-10-20 19:02:19 +0000

    booleans 2

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -15,13 +15,15 @@ module.exports.inBetween = function(lower, middle, upper) {
 };
 
 module.exports.outsideRanges = function(number) {
-    return !(!(10 <= number <= 20) || !(42 < number <= 75) || !(1 < number < 6))
+//    return (!(10 <= number <= 20) && !(42 < number <= 75) && !(1 < number < 6));
 
-
+    return (!(number >= 10 && number <= 20) &&
+        !(number > 42 && number <= 75) &&
+        !(number > 1 && number < 6));
 
 };
 
 module.exports.nameAndPrice = function(name, price) {
-    return ( (name= ("NYTimes" || "LATimes")) && (price >= 1))
+    return ((name === 'NYTimes' || name === 'LATimes') && price >= 1);
 
 };
