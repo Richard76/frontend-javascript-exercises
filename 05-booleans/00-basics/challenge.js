e59c78b593f59a106c4f4a5f92f51bb757cd0d93
Richard <RichardFarr7@gmail.com>
2014-10-20 03:31:30 +0000

    booleans 1

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,27 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
+    return (stringOne == stringTwo);
+
 
 };
 
 module.exports.notEqual = function(one, two) {
+    return (one !== two);
 
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
+    return (lower < middle && middle < upper);
 
 };
 
 module.exports.outsideRanges = function(number) {
+    return !(!(10 <= number <= 20) || !(42 < number <= 75) || !(1 < number < 6))
+
+
 
 };
 
 module.exports.nameAndPrice = function(name, price) {
+    return ( (name= ("NYTimes" || "LATimes")) && (price >= 1))
 
 };
